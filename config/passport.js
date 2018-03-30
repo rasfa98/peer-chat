1d48b943c97f4d1860907ef3de3c26d802990843
Rasmus Falk <rf222fu@student.lnu.se>
2018-03-30 15:30:05 +0200

    Saving google-users to DB

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+/**
+ * Passport configuration.
+ *
+ * @module config/passport.js
+ * @author Rasmus Falk
+ * @version 1.0.0
+ */
+
+'use strict'
+
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+const User = require('../models/User')
+
+module.exports.run = () => {
+  passport.use(new GoogleStrategy({
+    callbackURL: '/auth/google/redirect',
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  }, async (accessToken, refreshToken, profile, done) => {
+    const current = await User.findOne({ googleId: profile.id })
+
+    if (current) {
+
+    } else {
+      const user = new User({
+        username: profile.displayName,
+        googleId: profile.googleId
+      })
+
+      user.save()
+    }
+  }))
+}
