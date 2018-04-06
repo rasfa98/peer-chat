74d15104502bbd74ccb064d13f8ecfeaa219a07f
Rasmus Falk <rf222fu@student.lnu.se>
2018-04-06 20:24:07 +0200

    Changing stun schemas

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -44,11 +44,11 @@ export class ChatComponent implements OnInit {
       reconnectTimer: 150,
       config: {
         iceServers: [{ urls:  [
-          'stun.l.google.com:19302',
-          'stun1.l.google.com:19302',
-          'stun2.l.google.com:19302',
-          'stun3.l.google.com:19302',
-          'stun4.l.google.com:19302' ]} 
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302' ]} 
         ]}
     })
 
