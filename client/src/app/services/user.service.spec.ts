19176fa9e36af866ba14435d0b78fa34723acc41
Rasmus Falk <rf222fu@student.lnu.se>
2018-04-08 12:04:23 +0200

    Adding file for storing interfaces and one unit-test

--- client/src/app/services/user.service.spec.ts
+++ client/src/app/services/user.service.spec.ts
@@ -1,15 +1,41 @@
+import { UserService } from "./user.service";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed, inject } from '@angular/core/testing';
-
-import { UserService } from './user.service';
+import { User } from '../models/user.model';
 
 describe('UserService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [UserService]
-    });
-  });
+    let service: UserService
+    let httpMock: HttpTestingController
 
-  it('should be created', inject([UserService], (service: UserService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [UserService]
+        })
+
+        service = TestBed.get(UserService)
+        httpMock = TestBed.get(HttpTestingController)
+    })
+
+    afterEach(() => {
+        httpMock.verify()
+    })
+
+    it('should get the current user', () => {
+        const dummyUser: User = {
+            id: '1234',
+            fullName: null,
+            email: null
+        }
+
+        service.getCurrentUser().subscribe(res => {
+            expect(res.id).toEqual('1234')
+        })
+
+        const request = httpMock.expectOne(`${service.ROOT_URL}/user/current`)
+
+        expect(request.request.method).toBe('GET')
+
+        request.flush(dummyUser)
+    })
+})
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.model';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UserService {
+  ROOT_URL = 'http://localhost:8000'
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getCurrentUser() {
-    return this.http.get('http://localhost:8000/user/current')
-    .map(user => user.json())
+    return this.http.get<User>(`${this.ROOT_URL}/user/current`)
   }
 }
