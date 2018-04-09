22928654157d45d80658077d613a44cef9267825
Rasmus Falk <rf222fu@student.lnu.se>
2018-04-09 22:03:53 +0200

    Active user will be shown in the feed component

--- client/src/app/services/chat.service.ts
+++ client/src/app/services/chat.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class ChatService {
+  private activeUserItem = new BehaviorSubject({ fullName: 'Messages' })
 
+  currentActiveUserItem = this.activeUserItem.asObservable()
+  
   constructor() { }
+
+  changeActiveUserItem(user) {
+    this.activeUserItem.next(user)
+  }
 
 }
