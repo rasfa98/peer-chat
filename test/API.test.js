e1ccef90957bcd7e2ea088fed2712cae7df5d1fe
Rasmus Falk <rf222fu@student.lnu.se>
2018-04-15 11:28:24 +0200

    Adding test for getting the current user

--- test/API.test.js
+++ test/API.test.js
@@ -3,16 +3,30 @@ const supertest = require('supertest')
 
 const server = supertest.agent('http://localhost:8000')
 
-describe('API', () => {
+describe('API /GET', () => {
 
-  describe('/GET /pagedoesnotexist', () => {
+  describe('/pagedoesnotexist', () => {
 
-    it('should return a 404 not found', done => {
+    it('should return 404 not found', done => {
       server
       .get('/pagedoesnotexist')
       .expect(404)
       .end((err, res) => {
         expect(res.status).to.equal(404)
+        expect(res.body.error).to.not.exist
+        done()
+      })
+    })
+  })
+
+  describe('user/current', () => {
+
+    it('should return 200', done => {
+      server
+      .get('/user/current')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200)
         expect(res.body.error).to.not.exist
         done()
       })
