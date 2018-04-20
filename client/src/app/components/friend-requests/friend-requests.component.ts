f77ce523204dd28c27e09f5368e796e5627c1ab6
Rasmus Falk <rf222fu@student.lnu.se>
2018-04-20 19:00:50 +0200

    Adding friend-requests component

---
+++ client/src/app/components/friend-requests/friend-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-requests',
+  templateUrl: './friend-requests.component.html',
+  styleUrls: ['./friend-requests.component.css']
+})
+export class FriendRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
