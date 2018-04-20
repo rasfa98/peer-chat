a7ea0f76eaabb3e1fe1567f2d23e8f9f74aa78a4
Rasmus Falk <rf222fu@student.lnu.se>
2018-04-20 18:27:24 +0200

    Will be able to send the queried users to the client

--- routes/user.js
+++ routes/user.js
@@ -9,9 +9,19 @@
 'use strict'
 
 const router = require('express').Router()
+const User = require('../models/User')
 
 router.route('/current')
     .get((req, res) => res.send({ id: res.locals.userId }))
+
+router.route('/fullName')
+    .post(async (req, res) => {
+      let users = await User.find({ fullName: req.body.fullName })
+
+      users = users.map(x => { return { id: x._id, fullName: x.fullName, email: x.email, status: x.status } })
+
+      res.send({ users: users })
+    })
 
 // Exports
 module.exports = router
