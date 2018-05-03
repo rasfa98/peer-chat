fa6babd66c6c391e9cde07bb463aec0a65b1d784
Rasmus Falk <rf222fu@student.lnu.se>
2018-05-03 20:12:52 +0200

    Adding popup for the dialer

--- client/src/app/components/popup/popup.component.ts
+++ client/src/app/components/popup/popup.component.ts
@@ -9,21 +9,31 @@ import { ChatService } from '../../services/chat.service'
 })
 export class PopupComponent implements OnInit {
   callInformation: object
+  dialInformation: object
+  calling: boolean
+  dialing: boolean
 
   constructor(private popupService: PopupService, private chatService: ChatService) { }
 
   ngOnInit() {
     // Observables.
     this.chatService.currentCallInformation.subscribe(callInformation => this.callInformation = callInformation)
+    this.chatService.currentDialInformation.subscribe(dialInformation => this.dialInformation = dialInformation)
+    this.chatService.currentCalling.subscribe(calling => this.calling = calling)
+    this.chatService.currentDialing.subscribe(dialing => this.dialing = dialing)
   }
 
   // Triggers answerCall() in feed-header component.
   answerCall() {
-    this.popupService.answerCallEvent()
+    this.popupService.answerCallEvent(true)
   }
 
   // Triggers hangUp() in feed-header component
   hangUp() {
-    this.popupService.hangUpEvent()
+    this.popupService.hangUpEvent(true)
+  }
+
+  cancelCall() {
+    this.popupService.cancelCallEvent(true)
   }
 }
