3c4e1b4d464eae147bd806cc15317a43a207786e
Rasmus Falk <rf222fu@student.lnu.se>
2018-05-07 07:42:41 +0200

    Adding current user component

---
+++ client/src/app/components/current-user/current-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-user',
+  templateUrl: './current-user.component.html',
+  styleUrls: ['./current-user.component.css']
+})
+export class CurrentUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
