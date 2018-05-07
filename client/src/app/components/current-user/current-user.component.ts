0380df072c9c744af62ef8069993c37b09b658b7
Rasmus Falk <rf222fu@student.lnu.se>
2018-05-07 07:58:30 +0200

    Displaying the signed in user above the search field

--- client/src/app/components/current-user/current-user.component.ts
+++ client/src/app/components/current-user/current-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { WebsocketService } from '../../services/websocket.service';
 
 @Component({
   selector: 'app-current-user',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-user.component.css']
 })
 export class CurrentUserComponent implements OnInit {
+  currentUser: any
+  socket: any
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private userService: UserService, private websocketService: WebsocketService) {
+    this.currentUser = { status: 'online', fullName: 'Rasmus Falk', avatar: '../../assets/avatars/avatar-3.png' }
   }
 
+  ngOnInit() {
+    this.socket = this.websocketService.socket
+
+    this.userService.getCurrentUser().subscribe(user => {
+      // this.currentUser = user
+      // this.socket.emit('newUser', user)
+    })
+
+    this.socket.on('updateCurrentUserStatus', status => {
+      this.currentUser.status = status.toUpperCase()
+    })
+  }
 }
