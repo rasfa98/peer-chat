e6ec4ec17b457349c56852b31fedd15cbf0baa7f
Rasmus Falk <rf222fu@student.lnu.se>
2018-05-17 07:53:42 +0200

    Adding flash message component

---
+++ client/src/app/components/flash-message/flash-message.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FlashMessageComponent } from './flash-message.component';
+
+describe('FlashMessageComponent', () => {
+  let component: FlashMessageComponent;
+  let fixture: ComponentFixture<FlashMessageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FlashMessageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FlashMessageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/flash-message/flash-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flash-message',
+  templateUrl: './flash-message.component.html',
+  styleUrls: ['./flash-message.component.css']
+})
+export class FlashMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
