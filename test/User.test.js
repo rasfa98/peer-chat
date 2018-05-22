d2907e82f1235b6b331a5b443af3321792f34ab4
Rasmus Falk <rf222fu@student.lnu.se>
2018-05-22 12:10:21 +0200

    Adding automated tests for the recently added validation

--- test/User.test.js
+++ test/User.test.js
@@ -1,15 +1,13 @@
 'use strict'
 
 const expect = require('chai').expect
-const describe = require('chai').describe
-const it = require('chai').it
 const User = require('../models/User')
 
 describe('User model validation', () => {
   it('should be able to register a user if all details are provided', done => {
     const user = new User({
       fullName: 'test',
-      password: 'test',
+      password: 'test123',
       email: 'test@test.se'
     })
 
@@ -22,7 +20,7 @@ describe('User model validation', () => {
   it('should not be able to register a user without a full name', done => {
     const user = new User({
       fullName: null,
-      password: 'test',
+      password: 'test123',
       email: 'test@test.se'
     })
 
@@ -32,11 +30,63 @@ describe('User model validation', () => {
     })
   })
 
-  it('should not be able to register a user without a email address', done => {
+  it('should not be able to register a user with a full name that contains invalid characters', done => {
+    const user = new User({
+      fullName: 'test123',
+      password: 'test123',
+      email: 'test@test.se'
+    })
+
+    user.validate(err => {
+      expect(err.errors.fullName).to.exist
+      done()
+    })
+  })
+
+  it('should not be able to register a user with a full name longer than 20 characters', done => {
+    const user = new User({
+      fullName: 'test123123123123123123123123123123123123',
+      password: 'test123',
+      email: 'test@test.se'
+    })
+
+    user.validate(err => {
+      expect(err.errors.fullName).to.exist
+      done()
+    })
+  })
+
+  it('should not be able to register a user without an email address', done => {
     const user = new User({
       fullName: 'test',
-      password: 'test',
+      password: 'test123',
       email: null
+    })
+
+    user.validate(err => {
+      expect(err.errors.email).to.exist
+      done()
+    })
+  })
+
+  it('should not be able to register a user with an email address longer than 50 characters', done => {
+    const user = new User({
+      fullName: 'test',
+      password: 'test123',
+      email: 'test@testtesttesttesttesttesttesttesttesttesttesttesttest.se'
+    })
+
+    user.validate(err => {
+      expect(err.errors.email).to.exist
+      done()
+    })
+  })
+
+  it('should not be able to register a user with an email address using a invalid format', done => {
+    const user = new User({
+      fullName: 'test',
+      password: 'test123',
+      email: 'test'
     })
 
     user.validate(err => {
@@ -49,6 +99,32 @@ describe('User model validation', () => {
     const user = new User({
       fullName: 'test',
       password: null,
+      email: 'test@test.se'
+    })
+
+    user.validate(err => {
+      expect(err.errors.password).to.exist
+      done()
+    })
+  })
+
+  it('should not be able to register a user with a password longer than 30 characters', done => {
+    const user = new User({
+      fullName: 'test',
+      password: 'test123123123123123123123123123123123123123123123123123123123123123123123123',
+      email: 'test@test.se'
+    })
+
+    user.validate(err => {
+      expect(err.errors.password).to.exist
+      done()
+    })
+  })
+
+  it('should not be able to register a user with a password shorter than 5 characters', done => {
+    const user = new User({
+      fullName: 'test',
+      password: 'test',
       email: 'test@test.se'
     })
 
